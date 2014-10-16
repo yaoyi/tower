4f60cc31eef9850219f8c5097ab0b84389f2c282
ben <ben@bmcd.co>
2014-10-17 02:52:04 +0800

    support group by date

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,8 +4,11 @@ class EventsController < ApplicationController
 	def index
 		@team = current_user.teams.find(params[:team_id])
 		@events = @team.events.page(params[:page])
-		@chunked_events = @events.chunk{|e| e.project}
+		# @date_chunked_events = @events.chunk{|e| e.project}
+		# @chunked_events = @events.chunk{|e| e.project}
+		@chunked_events = @events.chunk{|e| e.created_at.to_date}
 		@next_page = params[:page].to_i + 1 unless params[:page].blank?
 		@last_project = params[:last_project] || @events.last.project.id.to_s
+		@last_date = params[:last_date] || @events.last.created_at.to_date.to_s
 	end
 end
